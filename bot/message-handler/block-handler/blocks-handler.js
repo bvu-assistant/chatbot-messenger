3688062f3dc2cb1780b322a3e68755d66b2f3b9b
Ocean <anhtuan3675@outlook.com>
2020-05-17 20:32:00 +0700

    Update

--- bot/message-handler/block-handler/blocks-handler.js
+++ bot/message-handler/block-handler/blocks-handler.js
@@ -7,10 +7,6 @@ let show_functions              = require('./collections/direct-replies/show-fun
 let show_headlines              = require('./collections/sending/send-headlines');
 let show_studentNews            = require('./collections/sending/send-student-news');
 let send_test_schedule          = require('./collections/sending/send-test-schedule');
-let send_following_elearning    = require('./collections/sending/send-following-elearning');
-let send_class_in_eDate         = require('./collections/sending/send-class-in-eDate');
-let inform_new_schedule         = require('./collections/sending/inform-new-eSchedule');
-let send_detail_eSubject        = require('./collections/sending/send-detail-eSubject');
 let send_liability              = require('./collections/sending/send-liability');
 let send_summary_marks          = require('./collections/sending/send-summary-marks');
 
@@ -23,8 +19,7 @@ module.exports =
 {
     defaut_answer, reply_to_hi, getstarted_message,show_functions, 
 
-    show_headlines, show_studentNews, send_test_schedule, send_following_elearning, 
-    send_class_in_eDate, inform_new_schedule, send_detail_eSubject, send_liability, send_summary_marks, 
+    show_headlines, show_studentNews, send_test_schedule, send_liability, send_summary_marks, 
 
     get_student_id, 
     continue_test_schedule
