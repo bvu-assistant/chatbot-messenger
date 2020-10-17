0466a73b2cdac7f3990ff5944f5800b21305a58c
iceStorm <anhtuan3675@outlook.com>
2020-10-17 18:00:48 +0700

    fix normal schedule empty responding

--- src/bot/blocks/sending/send-normal-schedule.js
+++ src/bot/blocks/sending/send-normal-schedule.js
@@ -8,11 +8,14 @@ async function send({cloner, studentId, type = nsh.ScheduleType.TODAY})
     let bot = await new Bot(null, cloner);
 
     
+    // bot.messageSender.sendSenderAction({recipientID: bot.sender.id, action: 'mark_seen'});
+    // bot.messageSender.sendText({recipientID: bot.sender.id, content: 'Đang xử lý...', typingDelay: 1.35});
+    // setTimeout(() => {
+    //     bot.messageSender.sendSenderAction({recipientID: bot.sender.id, action: 'mark_seen'});
+    //     bot.messageSender.sendSenderAction({recipientID: bot.sender.id, action: 'typing_on'});
+    // }, 2000);
     bot.messageSender.sendSenderAction({recipientID: bot.sender.id, action: 'mark_seen'});
-    bot.messageSender.sendText({recipientID: bot.sender.id, content: 'Đang xử lý...', typingDelay: 1.35});
-    setTimeout(() => {
-        bot.messageSender.sendSenderAction({recipientID: bot.sender.id, action: 'typing_on'});
-    }, 2000);
+    bot.messageSender.sendSenderAction({recipientID: bot.sender.id, action: 'typing_on'});
 
 
     let messages = await nsh.renderNormalSchedulesTemplate({studentId: studentId, type: type});
